16df5517dde4933c15ad84d74ac18b5d68877d7b
darvesh <hello@darve.sh>
2021-02-21 00:24:49 +0530

    rewrite: reddit handler

--- src/constant.ts
+++ src/constant.ts
@@ -1,8 +1,7 @@
-export const INSTAGRAM_REGEX = /instagram/;
-export const REDDIT_REGEX = /redd\.it|^((https\:\/\/)?(www\.)?)?(old\.)?reddit\.com\/r\/\w+\/comments\/\w+\/\w+\/?/g;
+export const INSTAGRAM_REGEX = /instagram.com\/p\/[\w-_]+/;
+export const REDDIT_REGEX = /redd\.it|^((https:\/\/)?(www\.)?)?(old\.)?reddit\.com\/r\/\w+\/comments\/\w+\/\w+\/?/g;
 export const REDDIT_REGEX_ONE = /redd\.it/;
-export const REDDIT_REGEX_TWO = /^((https\:\/\/)?(www\.)?)?(old\.)?reddit\.com\/r\/\w+\/comments\/\w+\/\w+\/?/g;
-
+export const REDDIT_REGEX_TWO = /^((https:\/\/)?(www\.)?)?(old\.)?reddit\.com\/r\/\w+\/comments\/\w+\/\w+\/?/g;
 export const ERRORS = {
 	INVALID_LINK: "Link is invalid or not supported",
 	INVALID_POST: "This link couldn't be processed, try sending other link",
@@ -11,6 +10,4 @@ export const ERRORS = {
 		"This post's media size is too large or this bot is not capable of fetching this post's media yet"
 } as const;
 
-export const headers = `"User-Agent:'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0'"`
-
-export const placeholder = "https://ibb.co/rvYSLg5";
+export const headers = `"User-Agent:'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0'"`;
--- src/handler/reddit.ts
+++ src/handler/reddit.ts
@@ -1,26 +1,33 @@
-import type { TelegrafContext } from "telegraf/typings/context";
+import type TelegrafContext from "telegraf/typings/context";
 
 import axios from "axios";
 import { Readable } from "stream";
 
 import { Reddit } from "../types";
+import { CustomError } from "../utils";
 import { downloadVideo } from "../ffmpeg";
 import { ERRORS, REDDIT_REGEX_ONE, REDDIT_REGEX_TWO } from "../constant";
 
-const resolveURL = (url: string) =>
-	axios.get(url).then(res => res.request?.res?.responseUrl as string);
+const resolveLink = async (url: string): Promise<string | undefined> => {
+	if (url.match(REDDIT_REGEX_ONE))
+		//resolving redd.it link to reddit.com link
+		return axios
+			.get(url)
+			.then(res => res.request?.res?.responseUrl as string | undefined);
+	return REDDIT_REGEX_TWO.exec(url)?.[0];
+};
 
 export const redditHandler = async (ctx: TelegrafContext) => {
+	if (!ctx.message) return;
+	if (!("text" in ctx.message)) {
+		throw new CustomError(ERRORS.INVALID_POST, "redditHandler: A");
+	}
 	const link = ctx.message?.text;
-	if (!link) throw new Error(ERRORS.INVALID_LINK);
+	if (!link) throw new CustomError(ERRORS.INVALID_LINK, "redditHandler: B");
 
-	const rawLink = link.match(REDDIT_REGEX_ONE)
-		? await resolveURL(link)
-		: link.match(REDDIT_REGEX_TWO)
-		? link
-		: "";
-
-	if (!rawLink) throw new Error(ERRORS.INVALID_LINK);
+	const rawLink = await resolveLink(link);
+	if (!rawLink)
+		throw new CustomError(ERRORS.INVALID_LINK, "redditHandler: C");
 
 	const ogLink = rawLink.endsWith("/")
 		? rawLink + ".json"
@@ -29,14 +36,13 @@ export const redditHandler = async (ctx: TelegrafContext) => {
 	const res = await axios.get(ogLink);
 	const reddit = res?.data?.[0]?.data?.children?.[0]?.data as Reddit;
 
-	if (!reddit) throw new Error(ERRORS.INVALID_LINK);
+	if (!reddit) throw new CustomError(ERRORS.INVALID_LINK, "redditHandler: D");
 
 	const { title } = reddit;
 
 	if (reddit.is_video) {
-
 		if (!reddit.media?.reddit_video.fallback_url)
-			throw new Error(ERRORS.INVALID_POST);
+			throw new CustomError(ERRORS.INVALID_POST, "redditHandler: E");
 
 		const media = await downloadVideo(
 			reddit.media?.reddit_video.fallback_url
@@ -47,22 +53,21 @@ export const redditHandler = async (ctx: TelegrafContext) => {
 
 		if (media instanceof Readable)
 			return ctx.replyWithVideo({ source: media }, { caption: title });
-			
-		throw new Error(ERRORS.INVALID_POST);
+
+		throw new CustomError(ERRORS.INVALID_POST, "redditHandler: F");
 	}
 
 	if (reddit.url.match(/\.(jpg|jpeg|png)/))
 		return ctx.replyWithPhoto(reddit.url, { caption: title });
 
 	if (reddit.url.match(/gfycat|\.gif/)) {
-
 		if (!reddit.preview?.reddit_video_preview.fallback_url)
-			throw new Error(ERRORS.INVALID_POST);
+			throw new CustomError(ERRORS.INVALID_POST);
 
 		return ctx.replyWithVideo(
 			reddit.preview?.reddit_video_preview.fallback_url,
 			{ caption: title }
 		);
 	}
-	throw new Error(ERRORS.INVALID_POST);
+	throw new CustomError(ERRORS.UNKNOWN, "redditHandler: G");
 };
