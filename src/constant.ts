7e900345427636ba40389072bd9a2dc0f9006e17
darvesh <hello@darve.sh>
2021-02-21 02:12:41 +0530

    fix: regex bug with global flag

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,7 @@
 export const INSTAGRAM_REGEX = /instagram.com\/p\/[\w-_]+/;
-export const REDDIT_REGEX = /redd\.it|^((https:\/\/)?(www\.)?)?(old\.)?reddit\.com\/r\/\w+\/comments\/\w+\/\w+\/?/g;
+export const REDDIT_REGEX = /redd\.it|^((https:\/\/)?(www\.)?)?(old\.)?reddit\.com\/r\/\w+\/comments\/\w+\/\w+\/?/;
 export const REDDIT_REGEX_ONE = /redd\.it/;
-export const REDDIT_REGEX_TWO = /^((https:\/\/)?(www\.)?)?(old\.)?reddit\.com\/r\/\w+\/comments\/\w+\/\w+\/?/g;
+export const REDDIT_REGEX_TWO = /^((https:\/\/)?(www\.)?)?(old\.)?reddit\.com\/r\/\w+\/comments\/\w+\/\w+\/?/;
 export const ERRORS = {
 	INVALID_LINK: "Link is invalid or not supported",
 	INVALID_POST: "This link couldn't be processed, try sending other link",
